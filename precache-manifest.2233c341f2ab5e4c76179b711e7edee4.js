19e72c8a033030b40ece79d2d2ca5363ef8b2f72
Fon To <fonto_85@hotmail.com>
2020-04-26 22:21:14 +0200

    upload

---
+++ precache-manifest.2233c341f2ab5e4c76179b711e7edee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0011ef77176a12f875bb8a5819da4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a4c0250628bcaf919c",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "44ae472ca3fbca03723a",
+    "url": "/static/js/2.11ed85f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11ed85f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a4c0250628bcaf919c",
+    "url": "/static/js/main.75da8262.chunk.js"
+  },
+  {
+    "revision": "b8fddef8d1b0d52aac21",
+    "url": "/static/js/runtime-main.181f76ac.js"
+  }
+]);
